db962f45f6a9f5000bf8cc5f1b15e4136b721d54
g.palyukh <g.palyukh@gmail.com>
2025-04-25 23:15:25 +0200

    Clone column from the table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const copyIndex = 2;
+const pasteIndex = 4;
+
+document.querySelectorAll(`tr > *:nth-child(${copyIndex})`).forEach((el, i) => {
+  const cloneThHead = el.cloneNode(true);
+  const parentNode = el.parentNode;
+
+  parentNode.insertBefore(cloneThHead, parentNode.cells[pasteIndex]);
+});
